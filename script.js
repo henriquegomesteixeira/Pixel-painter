de488d3fb2f3e24616e4600327d41117a94d5d17
henriquegomesteixeira <henriquegomes080012@gmail.com>
2023-10-14 20:09:29 -0300

    Responsividade

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const buttonRandonColors = document.querySelector('#button-random-color');
 const colors = document.querySelectorAll('.color');
 const sectionPixel = document.querySelector('#pixel-board');
+
 let colorSelected = 'black';
 
 // Ao reiniciar a página, as cores da paleta de cores são mantidas através do localStorage
@@ -30,108 +31,177 @@ buttonRandonColors.addEventListener('click', () => {
   localStorage.setItem('colorPalette', JSON.stringify(palette));
 });
 
-// Define a largura do quadro de pixels
-function widthPixelBoard(number) {
-  const widths = {
-    1: '210px',
-    2: '210px',
-    3: '210px',
-    4: '210px',
-    5: '210px',
-    6: '252px',
-    7: '294px',
-    8: '336px',
-    9: '378px',
-    10: '420px',
-    11: '462px',
-    12: '504px',
+// Define a largura do quadro de pixels de acordo com o tamanho do quadro
+document.addEventListener('DOMContentLoaded', () => {
+  function widthPixelBoard(number) {
+    const screenWidth = window.innerWidth;
+
+    if (screenWidth < 580) {
+      const widths = {
+        1: '110px',
+        2: '110px',
+        3: '110px',
+        4: '110px',
+        5: '110px',
+        6: '132px',
+        7: '154px',
+        8: '176px',
+        9: '198px',
+        10: '220px',
+        11: '242px',
+        12: '264px',
+      };
+      
+      return sectionPixel.style.width = widths[number] || '';
+    }
+
+    const widths = {
+      1: '210px',
+      2: '210px',
+      3: '210px',
+      4: '210px',
+      5: '210px',
+      6: '252px',
+      7: '294px',
+      8: '336px',
+      9: '378px',
+      10: '420px',
+      11: '462px',
+      12: '504px',
+    };
+
+    sectionPixel.style.width = widths[number] || '';
+  }
+
+  // Cria os pixels do quadro de pixels, armazena no localStorage e define o tamanho do quadro
+  let storageRead = false;
+  const createBoard = (numberCreatePixels) => {
+    let numberPixels = numberCreatePixels;
+
+    if (localStorage.getItem('boardSize') !== null && storageRead === false) {
+      const boardSize = localStorage.getItem('boardSize');
+      numberPixels = boardSize * boardSize;
+      widthPixelBoard(boardSize);
+
+      const inputBoardSize = document.querySelector('#board-size');
+      inputBoardSize.value = boardSize;
+      storageRead = true;
+    } else {
+      numberPixels = numberCreatePixels * numberCreatePixels;
+      console.log('numberPixels', numberPixels);
+      if (!numberCreatePixels || numberCreatePixels === '' || numberCreatePixels === null) { numberPixels = 81; }
+      widthPixelBoard(numberCreatePixels);
+      const inputBoardSize = document.querySelector('#board-size');
+      inputBoardSize.value = numberPixels === 81 ? 9 : numberCreatePixels;
+    }
+
+    for (let index = 0; index < numberPixels; index += 1) {
+      const pixel = document.createElement('div');
+      pixel.className = 'pixel';
+      sectionPixel.appendChild(pixel);
+    }
   };
+  createBoard();
 
-  sectionPixel.style.width = widths[number] || '';
-}
+  // Muda a cor do pixel ao clicar e arrastar o mouse
+  function setPixelEventListeners() {
+    let painting = false;
 
-// Cria os pixels do quadro de pixels e armazena no localStorage
-const createBoard = (numberCreatePixels) => {
-  let numberPixels = numberCreatePixels;
+    document.querySelectorAll('.pixel').forEach((pixel, index) => {
+      pixel.addEventListener('click', (event) => {
+        const clickedPixel = event.target;
+        clickedPixel.style.backgroundColor = colorSelected;
 
-  if (!numberCreatePixels) { numberPixels = 81; }
-  if (localStorage.getItem('boardSize') !== null) {
-    const boardSize = localStorage.getItem('boardSize');
-    numberPixels = boardSize * boardSize;
-    widthPixelBoard(boardSize);
+        // Salva o estado do quadro de pixels no localStorage
+        const pixelBoardState = JSON.parse(localStorage.getItem('pixelBoard')) || {};
+        pixelBoardState[index] = colorSelected;
+        localStorage.setItem('pixelBoard', JSON.stringify(pixelBoardState));
+      });
 
-    const inputBoardSize = document.querySelector('#board-size');
-    inputBoardSize.value = boardSize;
-  }
+      // Pinta o pixel ao arrastar o mouse
+      pixel.addEventListener('mousedown', () => {
+        painting = true;
+      });
+      pixel.addEventListener('mousemove', (event) => {
+        if (painting) {
+          const clickedPixel = event.target;
+          clickedPixel.style.backgroundColor = colorSelected;
+  
+          const pixelBoardState = JSON.parse(localStorage.getItem('pixelBoard')) || {};
+          pixelBoardState[index] = colorSelected;
+          localStorage.setItem('pixelBoard', JSON.stringify(pixelBoardState));
+        }
+      });
+      pixel.addEventListener('touchstart', () => {
+        painting = true;
+      });
+      pixel.addEventListener('touchmove', (event) => {
+        event.preventDefault();
+        if (painting) {
+          const clickedPixel = event.target;
+          clickedPixel.style.backgroundColor = colorSelected;
 
-  for (let index = 0; index < numberPixels; index += 1) {
-    const pixel = document.createElement('div');
-    pixel.className = 'pixel';
-    sectionPixel.appendChild(pixel);
-  }
-};
-createBoard();
+          const pixelBoardState = JSON.parse(localStorage.getItem('pixelBoard')) || {};
+          pixelBoardState[index] = colorSelected;
+          localStorage.setItem('pixelBoard', JSON.stringify(pixelBoardState));
+        }
+      });
+      document.addEventListener('mouseup', () => {
+        painting = false;
+      });
+      document.addEventListener('touchend', () => {
+        painting = false;
+      });
+      
 
-// Muda a cor do pixel ao clicar e armazena no localStorage
-function setPixelEventListeners() {
-  document.querySelectorAll('.pixel').forEach((pixel, index) => {
-    pixel.addEventListener('click', (event) => {
-      const clickedPixel = event.target;
-      clickedPixel.style.backgroundColor = colorSelected;
-
-      // Salva o estado do quadro de pixels no localStorage
+      // Verifica se há cor salva no localStorage para esse pixel
       const pixelBoardState = JSON.parse(localStorage.getItem('pixelBoard')) || {};
-      pixelBoardState[index] = colorSelected;
-      localStorage.setItem('pixelBoard', JSON.stringify(pixelBoardState));
+      const savedColor = pixelBoardState[index];
+      const clickedPixel = pixel;
+      clickedPixel.style.backgroundColor = savedColor;
+    });
+  }
+  setPixelEventListeners();
+
+  // Adiciona a classe 'selected' a cor selecionada
+  document.querySelectorAll('.color').forEach((cor) => {
+    cor.addEventListener('click', (event) => {
+      const selectedColor = document.querySelector('.selected');
+      selectedColor.classList.remove('selected');
+      event.target.classList.add('selected');
+      colorSelected = event.target.style.backgroundColor;
+    });
+  });
+
+  // Limpa o quadro de pixels e o localStorage
+  function clearPixelBoard() {
+    document.querySelectorAll('.pixel').forEach((pixel) => {
+      const clickedPixel = pixel;
+      clickedPixel.style.backgroundColor = 'white';
     });
 
-    // Verifica se há cor salva no localStorage para esse pixel
-    const pixelBoardState = JSON.parse(localStorage.getItem('pixelBoard')) || {};
-    const savedColor = pixelBoardState[index];
-    const clickedPixel = pixel;
-    clickedPixel.style.backgroundColor = savedColor;
+    localStorage.removeItem('pixelBoard');
+  }
+  document.querySelector('#clear-board').addEventListener('click', () => {
+    clearPixelBoard();
   });
-}
-setPixelEventListeners();
 
-// Adiciona a classe 'selected' a cor selecionada
-document.querySelectorAll('.color').forEach((cor) => {
-  cor.addEventListener('click', (event) => {
-    const selectedColor = document.querySelector('.selected');
-    selectedColor.classList.remove('selected');
-    event.target.classList.add('selected');
-    colorSelected = event.target.style.backgroundColor;
+  // Cria um novo quadro de pixels e limpa o quadro anterior
+  document.querySelector('#generate-board').addEventListener('click', () => {
+    const inputBoardSize = document.querySelector('#board-size');
+    let boardSize = inputBoardSize.value;
+
+    if (!boardSize) { alert('Valor inválido!'); return; }
+    if (boardSize < 5) { boardSize = 5; alert('Valor mínimo é 5!'); }
+    if (boardSize > 12) { boardSize = 12; alert('Valor máximo é 12!'); }
+
+    document.querySelectorAll('.pixel').forEach((pixel) => (pixel.remove()));
+
+    widthPixelBoard(boardSize);
+    createBoard(boardSize);
+    setPixelEventListeners();
+    clearPixelBoard();
+    localStorage.setItem('boardSize', boardSize);
   });
 });
 
-// Limpa o quadro de pixels e o localStorage
-function clearPixelBoard() {
-  document.querySelectorAll('.pixel').forEach((pixel) => {
-    const clickedPixel = pixel;
-    clickedPixel.style.backgroundColor = 'white';
-  });
-
-  localStorage.removeItem('pixelBoard');
-}
-document.querySelector('#clear-board').addEventListener('click', () => {
-  clearPixelBoard();
-});
-
-// Cria um novo quadro de pixels e limpa o quadro anterior
-document.querySelector('#generate-board').addEventListener('click', () => {
-  const inputBoardSize = document.querySelector('#board-size');
-  let boardSize = inputBoardSize.value;
-
-  if (!boardSize) { alert('Valor inválido!'); }
-  if (boardSize < 5) { boardSize = 5; alert('Valor mínimo é 5!'); }
-  if (boardSize > 12) { boardSize = 12; alert('Valor máximo é 12!'); }
-
-  document.querySelectorAll('.pixel').forEach((pixel) => (pixel.remove()));
-
-  const numberCreatePixels = boardSize * boardSize;
-  widthPixelBoard(boardSize);
-  createBoard(numberCreatePixels);
-  setPixelEventListeners();
-  clearPixelBoard();
-  localStorage.setItem('boardSize', boardSize);
-});
